9adcb5da07845bec1c1b7b1200efafecc5ab0f0e
Julie Belleville <35176177+jbellevi@users.noreply.github.com>
2019-10-23 08:25:55 -0700

    Initial commit

---
+++ src/main/java/powergrid/PowerGrid.java
@@ -0,0 +1,22 @@
+package powergrid;
+
+public class PowerGrid {
+
+	/**
+	 * Given loads a, b, and c, at three power stations, is it possible to
+	 * balance the load using the permitted operation?
+	 * 
+	 * @param a
+	 *            the load at Power Station A, a >= 0
+	 * @param b
+	 *            the load at Power Station B, b >= 0
+	 * @param c
+	 *            the load at Power Station C, c >= 0
+	 * @return true if load balancing is possible and false otherwise
+	 */
+	public static boolean canLoadBalance(int a, int b, int c) {
+		// TODO: implement this method
+		return false;
+	}
+
+}
